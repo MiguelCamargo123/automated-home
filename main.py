075ef27a7bc167d8d589bc59c0a187026b43732f
Miguel Camargo <omiguelcamargo@gmail.com>
2026-07-15 10:02:02 -0300

    Função interna de avançar no tempo criada

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import random
 
 
-class casa:
+class Casa:
     def __init__(self, temperatura: int, janela: bool, luz: bool) -> None:
         self.temperatura = temperatura
         self.janela = janela
         self.luz = luz
         self.hora = 6
         self.minuto = 0
+
+    def _avançar_tempo(self, minutos: int) -> None:
+        self.minuto += minutos
+
+        while self.minuto >= 60:
+            self.minuto -= 60
+            self.hora += 1
+
+        if self.hora >= 24:
+            self.hora = 0
