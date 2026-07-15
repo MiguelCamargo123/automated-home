8f2d880b0b91da3e317d93b86332f27817370753
Miguel Camargo <omiguelcamargo@gmail.com>
2026-07-15 10:19:33 -0300

    Função de ligar a luz ligada

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import random
 
 
 class Casa:
-    def __init__(self, temperatura: int, janela: bool, luz: bool) -> None:
-        self.temperatura = temperatura
+    def __init__(self, janela: bool, luz: bool) -> None:
+        self.temperatura = 23
         self.janela = janela
         self.luz = luz
         self.hora = 6
@@ -18,3 +18,13 @@ class Casa:
 
         if self.hora >= 24:
             self.hora = 0
+
+    def ligar_luz(self) -> None:
+        aumento_temp = self.temperatura + random.randint(-1, 1)
+        self.temperatura = aumento_temp
+
+        print('A luz foi ligada!!!')
+
+        print(f'A temperatura atual é: {self.temperatura}')
+
+        self._avançar_tempo(2)
